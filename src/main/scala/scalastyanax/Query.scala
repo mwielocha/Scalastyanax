a3ad2b1a254055e564062626f850e6e2d62350d3
Mikołaj Wielocha <mikolaj.wielocha@opigram.com>
2013-08-29 17:21:47 +0200

    More on return type

--- src/main/scala/scalastyanax/Query.scala
+++ src/main/scala/scalastyanax/Query.scala
@@ -27,7 +27,7 @@ case class ColumnFamilyQuery[K, C](astx: astxq.ColumnFamilyQuery[K, C]) {
     RowQuery(astx.getRow(key))
   }
 
-  def <-? (key: K) = one(key)
+  def <-? (key: K): RowQuery[K, C] = one(key)
 
   def slice(keys: Iterable[K]): RowSliceQuery[K, C] = {
     RowSliceQuery(astx.getRowSlice(keys))
@@ -46,7 +46,7 @@ case class RowQuery[K, C](astx: astxq.RowQuery[K, C]) {
 
   def <-? (key: C): Execution[Column[C]] = one(key)
 
-  def range(selector: Range[C]) = {
+  def range(selector: Range[C]): RowQuery[K, C] = {
     RowQuery(astx.withColumnRange(
       selector.firstOrNull,
       selector.lastOrNull,
@@ -66,7 +66,7 @@ case class RowQuery[K, C](astx: astxq.RowQuery[K, C]) {
 
   def </? (selector: Iterable[C]): RowQuery[K, C] = slice(selector)
 
-  def execute = {
+  def execute: Execution[Columns[C]] = {
     Execution.wrap(Columns(astx.execute().getResult))
   }
 
@@ -95,7 +95,7 @@ case class RowSliceQuery[K, C](astx: astxq.RowSliceQuery[K, C]) {
 
   def </? (selector: Iterable[C]): RowSliceQuery[K, C] = slice(selector)
 
-  def execute = {
+  def execute: Execution[Rows[K, C]] = {
     Execution.wrap(Rows(astx.execute().getResult))
   }
 
