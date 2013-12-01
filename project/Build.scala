91878736b34071c5fa22de2414711ff6c162e731
mwielocha <mikowaj@gmail.com>
2013-12-01 15:28:40 +0100

    move astyanax dep to scope 'provided'

--- project/Build.scala
+++ project/Build.scala
@@ -4,16 +4,16 @@ import Keys._
 object ApplicationBuild extends Build {
 
   object V {
-    val astyanax = "1.56.*"
+    val astyanax = "1.56.+"
   }
 
   val appName         = "Scalastyanax"
   val appVersion      = "2.1.0-SNAPSHOT"
 
   val appDependencies =  Seq(
-    "com.netflix.astyanax" % "astyanax-core" % V.astyanax /*exclude("org.slf4j", "slf4j-log4j12")*/,
-    "com.netflix.astyanax" % "astyanax-thrift" % V.astyanax exclude("javax.servlet", "servlet-api"),
-    "com.netflix.astyanax" % "astyanax-entity-mapper" % V.astyanax /*exclude("org.slf4j", "slf4j-log4j12")*/,
+    "com.netflix.astyanax" % "astyanax-core" % V.astyanax /*exclude("org.slf4j", "slf4j-log4j12")*/ % "provided",
+    "com.netflix.astyanax" % "astyanax-thrift" % V.astyanax % "provided" exclude("javax.servlet", "servlet-api"),
+    "com.netflix.astyanax" % "astyanax-entity-mapper" % V.astyanax % "provided" /*exclude("org.slf4j", "slf4j-log4j12")*/,
     "com.google.code.findbugs" % "jsr305" % "1.3.+",
     /*"org.cassandraunit" % "cassandra-unit" % "1.1.1.2" % "test"
       exclude("org.apache.cassandra", "cassandra-all") exclude("org.hectorclient", "hector-core"),*/
